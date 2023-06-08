9202f4d8c3de8bb4f91fd5fd8176ac61aa8d77a3
HalCodes <hal@thinkflo.com>
2023-06-08 17:19:33 +0200

    Add to Body

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,14 +1,30 @@
 import Top_Nav from "@/components/Global_Components/Top_Nav";
 import Footer from "@/components/Global_Components/Footer";
- 
+import Script from 'next/script'
+import Head from "next/head";
+
 const Layout = ({ children, topNav, footer }) => {
 
   return (
   <>
+
     <a className="top-0 left-1/2 -translate-x-1/2 bg-indigo-500 text-white p-3 transform -translate-y-full z-50 absolute focus:translate-y-0" href="#main">Skip to Main Content</a>
     <Top_Nav blok={topNav} />
       {children}
     <Footer blok={footer} />
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-85JVV1G3YJ"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-85JVV1G3YJ');
+      `}
+    </Script>    
   </>
   )
 };
--- pages/_app.js
+++ pages/_app.js
@@ -9,8 +9,6 @@ import Team_Section from "@/components/Content_Blocks/Team_Section";
 import Pricing_Table from "@/components/Content_Blocks/Pricing_Table";
 import Feature_Section from "@/components/Content_Blocks/Feature_Section";
 import Accordion from "@/components/Content_Blocks/Accordion";
-import Script from 'next/script'
-import Head from "next/head";
 
 const components = {
   Hero,
@@ -37,21 +35,6 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
-    <Head>
-    <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-85JVV1G3YJ"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-85JVV1G3YJ');
-        `}
-      </Script>
-    </Head>
     <Layout {...pageProps}>
       <Component {...pageProps} />
     </Layout>
