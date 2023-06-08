489890a5b6623f297abc6eb6a2a16ca7173cae09
HalCodes <hal@thinkflo.com>
2023-06-08 09:57:59 +0200

    Update Heading elements in Featured Section for a11y

--- components/Content_Blocks/Feature_Section.js
+++ components/Content_Blocks/Feature_Section.js
@@ -28,10 +28,10 @@ const Feature_Section = ({blok}) => {
     <div className={`${colourSchemes[blok?.Colour_Scheme]?.background || "bg-gray-900"} py-24 sm:py-32`} {...storyblokEditable(blok)}>
       <Constrained_Width>
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className={`text-base font-semibold leading-7 text-indigo-400`}>{blok?.Title}</h2>
-          <p className={`mt-2 text-3xl font-bold tracking-tight sm:text-4xl ${colourSchemes[blok?.Colour_Scheme]?.subtitle || "text-white"}`}>
+          <p className={`text-base font-semibold leading-7 text-indigo-400`}>{blok?.Title}</p>
+          <h2 className={`mt-2 text-3xl font-bold tracking-tight sm:text-4xl ${colourSchemes[blok?.Colour_Scheme]?.subtitle || "text-white"}`}>
             {blok?.Subtitle}
-          </p>
+          </h2>
           <div className={`mt-6 text-lg leading-8 ${colourSchemes[blok?.Colour_Scheme]?.blurb || "text-gray-300"}`}>
             {render(blok?.Blurb)}
           </div>
