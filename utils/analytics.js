a573fc1b0f5dfa06eea3363ea6fc74d83740f250
HalCodes <hal@thinkflo.com>
2023-06-08 16:05:17 +0200

    Update GTM ID

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,6 +1,6 @@
 import Analytics from 'analytics'
 import googleTagManager from '@analytics/google-tag-manager'
-const GTM_CONTAINER_ID = 'G-0XY6T4N4BF'
+const GTM_CONTAINER_ID = 'G-85JVV1G3YJ'
 
 const analytics = Analytics({
     app: 'pixelpusher-prototype', // Call this whatever you like.
