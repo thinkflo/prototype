b6430c9f702261694b1ddf8bbdc18f247a36f01c
HalCodes <hal@thinkflo.com>
2023-06-08 08:49:09 +0200

    Add locales

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ["en-CA", "fr-CA"],
+    defaultLocale: "en-CA",
+  },  
   reactStrictMode: true,
   images: {
     unoptimized: true,
