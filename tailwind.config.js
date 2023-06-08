d71d090b66b2110a5edcaed76c5797702cf101ba
HalCodes <hal@thinkflo.com>
2023-06-08 09:03:16 +0200

    Change Indigo 500 for WCAG AAA contrast compliancy

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        indigo: {
+          500: "#3134F2"
+        }
+      }
+    },
   },
   plugins: [],
 }
