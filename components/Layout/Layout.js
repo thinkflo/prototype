dd8deb6f7b8caef37e6dec2a3892d81086e40c35
HalCodes <hal@thinkflo.com>
2023-06-08 10:09:17 +0200

    Add Jump Link

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -5,6 +5,7 @@ const Layout = ({ children, topNav, footer }) => {
 
   return (
   <>
+    <a className="top-0 left-1/2 -translate-x-1/2 bg-indigo-500 text-white p-3 transform -translate-y-full z-50 absolute focus:translate-y-0" href="#main">Skip to Main Content</a>
     <Top_Nav blok={topNav} />
       {children}
     <Footer blok={footer} />
