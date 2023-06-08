b2a68b4f5705f92df8515bd544eb84bee122a096
HalCodes <hal@thinkflo.com>
2023-06-08 09:27:46 +0200

    Meta tags for all pages

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -12,7 +12,38 @@ export default function Page({ story, preview }) {
 	return (
 		<>
       <Head>
-        <title>Pixelpusher Prototype</title>
+				<meta charset="utf-8"/>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
+				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+				<meta property="og:type" content="website"/>    <meta property="og:title" content="Non-Profit, Charitable Organization | Christian Horizons"/>
+				<meta property="og:url" content="https://christianhorizons.org/"/>
+				<meta property="og:description" content="Nurturing communities of belonging since 1965."/>
+				<meta property="description" content="Nurturing communities of belonging since 1965."/>
+
+				<meta name="twitter:site" content="@chorizons"/>
+				<meta name="twitter:creator" content="@chorizons"/>
+				<meta name="twitter:card" content="summary_large_image"/>
+				<meta name="twitter:title" content="Non-Profit, Charitable Organization | Christian Horizons"/>
+				<meta name="twitter:url" content="https://christianhorizons.org/"/>
+				<meta name="twitter:description" content="Nurturing communities of belonging since 1965."/>
+
+				<meta property="og:image" content=""/>
+				<meta name="twitter:image:src" content=""/>
+
+				<title>Non-Profit, Charitable Organization | Christian Horizons</title>
+
+				<meta name='robots' content='noindex, nofollow' />
+
+				<meta name="description" content="As a non-profit, developmental services organization, Christian Horizons helps people experiencing disabilities accomplish their goals &amp; thrive in communities." />
+				<link rel="canonical" href="https://christianhorizons.org/" />
+				<meta property="og:locale" content="en-CA" />
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="Non-Profit, Charitable Organization | Christian Horizons" />
+				<meta property="og:description" content="As a non-profit, developmental services organization, Christian Horizons helps people experiencing disabilities accomplish their goals &amp; thrive in communities." />
+				<meta property="og:url" content="https://christianhorizons.org/" />
+				<meta property="og:site_name" content="ChristianHorizons" />
+				<meta property="article:modified_time" content="2023-03-08T19:14:17+00:00" />
+				<meta name="twitter:card" content="summary_large_image" />
       </Head> 		
 			{story?.content ? (
 				<StoryblokComponent
